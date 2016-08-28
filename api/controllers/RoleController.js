6613a31391cd8b7e13a258f4672e457d5655b36a
Fabio Muramatsu <fabio.muramatsu@outlook.com>
2016-08-28 01:32:56 -0300

    Refactored code to insert role for improved reliability

--- api/controllers/RoleController.js
+++ api/controllers/RoleController.js
@@ -7,12 +7,23 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+check_params = function(req){
+	if(!req.param('name') || !req.param('where') || !req.param('when') ||
+		!req.param('cost') || !req.param('members') || !req.param('coordinator'))
+		return false;
+	else return true;
+}
+
 module.exports = {
 	create: (req, res) => {
 		coordinator = User.find({user_id: req.param('coordinator')}).exec((err, records) => {
 			if(err){
 				res.status = 400;
 				return res.send("Invalid coordinator");
+			}
+			if(!check_params(req)){
+				res.status = 400;
+				return res.send("Invalid JSON");
 			}
 
 			obj = {};
@@ -21,22 +32,21 @@ module.exports = {
 			obj.when = req.param('when');
 			obj.cost = req.param('cost');
 			obj.coordinator = records[0];
-			Role.create(obj).exec((err, role) => {
+			User.find({user_id: JSON.parse(req.param('members'))}).exec((err, users) => {
 				if(err){
 					res.status(400);
-					return res.send("Error creating role: " + err);
+					return res.send("Error creating users for role: " + err);
 				}
-				User.find({user_id: JSON.parse(req.param('members'))}).exec((err, users) => {
+				Role.create(obj).exec((err, role) => {
 					if(err){
 						res.status(400);
-						return res.send("Error creating users for role: " + err);
+						return res.send("Error creating role: " + err);
 					}
 					payments = [];
 					for(var user of users){
 						payments.push({payment_user: user, payment_role: role, value: 0});
 					}
 					Payment.create(payments).exec((err, created_payments) => {
-						console.log(created_payments);
 						if(err){
 							res.status(400);
 							return res.send("Error creating payment: " + err);
@@ -45,12 +55,6 @@ module.exports = {
 					});
 				});
 			});
-
 		});
-
-
-
-
-
 	}
 };
